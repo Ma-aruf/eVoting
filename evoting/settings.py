8664d93a4676fb9bbd6c325e6926eb1e2b4090db
Ma-aruf Meek <ma-aruf@outlook.com>
2026-09-20 14:25:12 +0000

    new

--- evoting/settings.py
+++ evoting/settings.py
@@ -216,7 +216,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5175",
     "http://127.0.0.1:5173",
-    "https://myevoting.onrender.com/",
+    "https://myevoting.onrender.com",
     "https://web-production-a9bba.up.railway.app"
 ]
 
@@ -252,7 +252,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5175",
     "http://127.0.0.1:5173",
-    "https://myevoting.onrender.com/",
+    "https://myevoting.onrender.com",
     "https://web-production-a9bba.up.railway.app"
 ]
 
